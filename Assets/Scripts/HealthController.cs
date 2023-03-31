17fbd5a76ffbad8863f5ff19b1ce6d7a567eec0c
Iancic <davidiancic05@gmail.com>
2023-03-31 19:58:59 +0300

    Health and Damage Scripts
    Am adaugat functiile de damage.

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public static HealthController instance;
+    public int current_health, max_health;
+
+    void Awake() 
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        current_health = max_health;
+    }
+
+    public void DealDamage()
+    {
+        current_health -= 1;
+
+        if (current_health <= 0)
+        {
+            current_health = 0;
+            gameObject.SetActive(false);
+        } 
+        //UIController.instance.UpdateHealthDisplay();
+    }
+}
